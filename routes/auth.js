1bba3191535e5f31dce903a700dc96b66c761905
jsalinas1616 <jsalinas@odintec.com.mx>
2021-08-10 19:14:28 -0500

    first deployment

--- routes/auth.js
+++ routes/auth.js
@@ -1,23 +1,18 @@
-/*
-    Rutas de Usuarios /Auth
-    host + /api/auth
-*/
-
-const { Router } = require("express");
-const { check } = require("express-validator");
+const { Router } = require('express');
+const { check } = require('express-validator');
 const router = Router();
-const { fieldValidate } = require("../middlewares/fieldsValidate");
+const { fieldValidate } = require('../middlewares/fieldsValidate');
 
-const { addUser, loginUser, revalidateToken } = require("../controllers/auth");
+const { addUser, loginUser, revalidateToken } = require('../controllers/auth');
 
-const { validateJWT } = require("../middlewares/revalidar-jwt");
+const { validateJWT } = require('../middlewares/revalidar-jwt');
 
 router.post(
-  "/adduser",
+  '/adduser',
   [
-    check("name", "El nombre es obligatorio").not().isEmpty(),
-    check("email", "El mail es obligatorio").not().isEmpty(),
-    check("password", "El password debe de ser de 6 caracteres").isLength({
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El mail es obligatorio').not().isEmpty(),
+    check('password', 'El password debe de ser de 6 caracteres').isLength({
       min: 6,
     }),
     fieldValidate,
@@ -26,14 +21,14 @@ router.post(
 );
 
 router.post(
-  "/",
+  '/',
   [
-    check("email", "El email es obligatorio").not().isEmpty(),
-    check("password", "Por favor escriba un password").isLength({ min: 6 }),
+    check('email', 'El email es obligatorio').not().isEmpty(),
+    check('password', 'Por favor escriba un password').isLength({ min: 6 }),
   ],
   loginUser
 );
 
-router.get("/renew", [validateJWT], revalidateToken);
+router.get('/renew', [validateJWT], revalidateToken);
 
 module.exports = router;
